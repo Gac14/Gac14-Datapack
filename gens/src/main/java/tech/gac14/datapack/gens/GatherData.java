b0e9fdeb634e587696116e372bbae80aa325a770
Connor Horman <chorman64@gmail.com>
2020-03-05 18:15:38 -0500

    Fix Some code

--- gens/src/main/java/tech/gac14/datapack/gens/GatherData.java
+++ gens/src/main/java/tech/gac14/datapack/gens/GatherData.java
@@ -1,12 +1,15 @@
 package tech.gac14.datapack.gens;
 
 
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+import tech.gac14.datapack.gens.loot.Gac14LootProvider;
 
 @Mod.EventBusSubscriber(modid="gac14-datapack-gen",bus= Mod.EventBusSubscriber.Bus.MOD)
 public class GatherData {
+    @SubscribeEvent
     public static void gatherData(GatherDataEvent event){
-
+        event.getGenerator().addProvider(new Gac14LootProvider(event.getGenerator()));
     }
 }
